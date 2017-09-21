ecd6d0359724ace9e696dc47ff120681309415f9
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-09-21 10:05:02 +0900

    add function adding user_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920175113) do
+ActiveRecord::Schema.define(version: 20170921002454) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170920175113) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
