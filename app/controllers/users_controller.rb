6b8b4f1071e2e9ca432543d4d79873d8de2cccc4
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-10-06 15:05:03 +0900

    redirect modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,7 +82,7 @@ class UsersController < ApplicationController
   def logout
     session[:user_id] = nil
     flash[:notice] = "ログアウトしました。"
-    redirect_to("/login")
+    redirect_to("/")
   end
 
 end
